10c05f37e3f0c7412e96f2f547706ca493c66a8e
ElectRICdll <2636422098@qq.com>
2023-04-26 11:07:57 +0800

    LogGenerate()

--- constants/values.go
+++ constants/values.go
@@ -1,7 +1,36 @@
 package constants
 
-const VERSION string = "b0.1"
+import (
+	"bytes"
+	"log"
+	"os"
+	"time"
+)
 
-// current version set
-const HOST string = ""
-const THENAME string = "KONN"
+const (
+	VERSION     string = "b0.1"
+	MODULE_NAME string = "KONN"
+)
+
+const (
+	INFO    = "[INFO]"
+	DEBUG   = "[DEBUG]"
+	WARNING = "[WARNING]"
+	ERROR   = "[ERROR]"
+	FATAL   = "[FATAL]"
+)
+
+var Host string = "106.53.70.120"
+var LogEvent chan string
+
+// Generate a log to your target string.
+func LogGenerate(msg string, logLevel string) string {
+	if msg == FATAL {
+		defer os.Exit(1)
+		defer time.Sleep(5 * 1e9)
+	}
+	buf := new(bytes.Buffer)
+	logger := log.New(buf, logLevel, log.Ldate|log.Ltime)
+	logger.Println(msg)
+	return buf.String()
+}
