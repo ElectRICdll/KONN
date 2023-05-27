ee013c5a0d60420a5314257616be21e304e7e4f4
ElectRICdll <2636422098@qq.com>
2023-05-28 01:47:16 +0800

    更名为constants

---
+++ constants/constants.go
@@ -0,0 +1,36 @@
+package constants
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"time"
+)
+
+const (
+	VERSION     string = "b0.1"
+	MODULE_NAME string = "KONN"
+)
+
+const (
+	INFO    = "[INFO]"
+	DEBUG   = "[DEBUG]"
+	WARNING = "[WARNING]"
+	ERROR   = "[ERROR]"
+	FATAL   = "[FATAL]"
+)
+
+var Host string = "106.53.70.120"
+var LogEvent chan string
+
+// Generate a log to your target string.
+func LogGenerate(msg string, logLevel string) string {
+	if msg == FATAL {
+		defer os.Exit(1)
+		defer time.Sleep(5 * 1e9)
+	}
+	buf := new(bytes.Buffer)
+	logger := log.New(buf, logLevel, log.Ldate|log.Ltime)
+	logger.Println(msg)
+	return buf.String()
+}
